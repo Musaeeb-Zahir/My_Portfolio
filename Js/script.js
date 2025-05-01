5b81a30334aa30fdcc9f83b64a99c7f63a5d015d
musaeeb-zahir <musaeeb-zahir>
2025-05-01 08:09:43 +0500

    update

--- Js/script.js
+++ Js/script.js
@@ -3,7 +3,13 @@ const cross = document.querySelector(".cross");
 const nav = document.querySelector(".nav");
 const navlinks = document.querySelector(".nav-links");
 ham.addEventListener("click", () => {
-  ham.style.display = " none !important";
+  ham.style.visibility = " hidden";
   cross.style.display = "block";
-  navList.style.transform = "translateX(0)";
+  navlinks.style.transform = "translateX(0)";
+});
+
+cross.addEventListener("click", () => {
+  ham.style.visibility = "visible";
+  cross.style.display = "none";
+  navlinks.style.transform = "translateX(-100%)";
 });
