96e940eec29e8a9291652f752199a18e485450a9
Musaeeb-zahir <musaeebzahir@gmail.com>
2025-05-03 12:36:00 +0500

    typing effect

---
+++ Js/typingEffect.js
@@ -0,0 +1,11 @@
+const typed = new Typed("#typed-text", {
+  strings: [
+    "Frontend Developer",
+    "JavaScript Enthusiast",
+    "Web Designer",
+    "Problem Solver",
+  ],
+  typeSpeed: 60,
+  backSpeed: 30,
+  loop: true,
+});
