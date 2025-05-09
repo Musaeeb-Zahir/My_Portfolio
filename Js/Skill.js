1c3f472c3c69dc779e759b407eb761c57a10d41e
Musaeeb-zahir <musaeebzahir@gmail.com>
2025-05-09 09:51:13 +0500

    set skills percentages

--- Js/Skill.js
+++ Js/Skill.js
@@ -1,5 +1,41 @@
+const skillContainer = document.querySelector(".skills-container");
+const skillArr = [
+  {
+    name: "HTML & CSS",
+    percent: 90,
+    icon: "<i class='bx bxl-html5'></i>",
+  },
+  {
+    name: "Tailwind CSS",
+    percent: 75,
+    icon: "<i class='bx bxl-css3'></i>",
+  },
+  {
+    name: "JavaScript",
+    percent: 80,
+    icon: "<i class='bx bxl-javascript'></i>",
+  },
+  {
+    name: "React",
+    percent: 75,
+    icon: "<i class='bx bxl-react'></i>",
+  },
+  {
+    name: "Node.js",
+    percent: 70,
+    icon: "<i class='bx bxl-nodejs'></i>",
+  },
+];
+skillArr.forEach((skill, index) => {
+  skillContainer.innerHTML += `  <div class="skill" data-percent=${skill.percent}>
+    <div class="circle-bg"><div class="circle-inner">
+    <p>Percent</p>
+    <div class="percent">${skill.percent}%</div>
+    </div></div>
+    <div class="label"><i class='bx bxl-nodejs'></i>${skill.name}</div>
+    </div>`;
+});
 const skills = document.querySelectorAll(".skill");
-
 skills.forEach((skill) => {
   const percent = skill.dataset.percent;
   const circle = skill.querySelector(".circle-bg");
