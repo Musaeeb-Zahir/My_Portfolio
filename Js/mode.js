75fd3ab1dcc9e465af119d49f25cb9da8feb6d9d
Musaeeb-zahir <musaeebzahir@gmail.com>
2025-05-28 00:32:42 +0500

    fix some bugs in style.css

--- Js/mode.js
+++ Js/mode.js
@@ -1,5 +1,18 @@
 const modeBtn = document.querySelector(".light-dark-mode");
+const root = document.documentElement;
+function enableLightMode() {
+  root.style.setProperty("--background-color", "#ffffff");
+  root.style.setProperty("--heading-text-color", "#161513");
+  root.style.setProperty("--header-background-color", "#bec1dd");
+  root.style.setProperty("--experience-text-color", "#0d1037");
+  const allElements = document.querySelectorAll("*");
+  allElements.forEach((el) => {
+    const style = window.getComputedStyle(el);
+    if (style.color === "rgb(255, 255, 255)") {
+      el.style.color = "black";
+    }
+  });
+}
 modeBtn.addEventListener("click", () => {
-  console.log("mode kam kr rha ha ", modeBtn);
-  
+  enableLightMode();
 });
