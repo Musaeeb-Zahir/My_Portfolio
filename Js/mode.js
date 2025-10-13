ffce405117c4641087e205f64b20c8e396850b4c
Musaeeb-zahir <musaeebzahir@gmail.com>
2025-10-13 22:58:10 +0500

    try to add some light mode

--- Js/mode.js
+++ Js/mode.js
@@ -1,17 +1,26 @@
 const modeBtn = document.querySelector(".light-dark-mode");
+const labelClr = document.querySelectorAll(".label");
+const aboutExpTxt = document.querySelectorAll(".about");
 const root = document.documentElement;
 function enableLightMode() {
+  console.log("click ho gia");
+
   root.style.setProperty("--background-color", "#ffffff");
   root.style.setProperty("--heading-text-color", "#161513");
-  root.style.setProperty("--header-background-color", "#bec1dd");
-  root.style.setProperty("--experience-text-color", "#0d1037");
-  const allElements = document.querySelectorAll("*");
-  allElements.forEach((el) => {
-    const style = window.getComputedStyle(el);
-    if (style.color === "rgb(255, 255, 255)") {
-      el.style.color = "black";
-    }
+  // root.style.setProperty("--experience-text-color", "#0d1037");
+  labelClr.forEach((label) => {
+    label.style.color = "#000";
   });
+  aboutExpTxt.forEach((txt) => {
+    txt.style.color = "#bec1dd";
+  });
+  const allElements = document.querySelectorAll("*");
+  //   allElements.forEach((el) => {
+  //     const style = window.getComputedStyle(el);
+  //     if (style.color === "rgb(255, 255, 255)") {
+  //       el.style.color = "black";
+  //     }
+  //   });
 }
 modeBtn.addEventListener("click", () => {
   enableLightMode();
